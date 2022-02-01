7db9f8ea168c9eada60491c1a0ca887d63a3ec26
Jeremy Ho <jujaga@gmail.com>
2022-01-31 17:14:01 -0800

    Add JSDoc for utils
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/objectStorage/utils.js
+++ app/src/components/objectStorage/utils.js
@@ -1,6 +1,12 @@
 const DELIMITER = '/';
 
 const utils = {
+  /**
+   * @function delimit
+   * Yields a string `s` that will always have a trailing delimiter. Returns an empty string if falsy.
+   * @param {string} The input string
+   * @returns {string} The string `s` with the trailing delimiter, or an empty string.
+   */
   delimit(s) {
     if (s) {
       return s.endsWith(DELIMITER) ? s : `${s}${DELIMITER}`;
@@ -8,6 +14,12 @@ const utils = {
     return '';
   },
 
+  /**
+   * @function join
+   * Joins a set of string arguments to yield a string path
+   * @param  {...string} items The strings to join on
+   * @returns {string} A path string with the specified delimiter
+   */
   join(...items) {
     if (items && items.length) {
       const parts = [];
@@ -23,7 +35,13 @@ const utils = {
     return '';
   },
 
-  // https://github.com/aws/aws-sdk-js-v3/issues/1877#issuecomment-755446927
+  /**
+   * @function streamToBuffer
+   * Reads a Readable stream, writes to and returns an array buffer
+   * @see https://github.com/aws/aws-sdk-js-v3/issues/1877#issuecomment-755446927
+   * @param {Readable} stream A readable stream object
+   * @returns {array} A buffer usually formatted as an Uint8Array
+   */
   streamToBuffer(stream) { // Readable
     return new Promise((resolve, reject) => {
       const chunks = []; // Uint8Array[]
