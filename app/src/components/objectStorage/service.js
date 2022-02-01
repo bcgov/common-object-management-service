cc1d4e2dc2cad0958633545a37ccf81a8cf3b5a1
Jeremy Ho <jujaga@gmail.com>
2022-01-31 17:14:27 -0800

    Add JSDocs for object storage service
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/objectStorage/service.js
+++ app/src/components/objectStorage/service.js
@@ -19,6 +19,13 @@ const accessKeyId = config.get('objectStorage.accessKeyId');
 const secretAccessKey = config.get('objectStorage.secretAccessKey');
 
 const objectStorageService = {
+  /**
+   * @private
+   * @property _s3Client
+   * The AWS S3Client used for interacting with S3 compatible storage
+   * @param {Readable} stream A readable stream object
+   * @returns {object} A pre-configured S3 Client object
+   */
   _s3Client: new S3Client({
     apiVersion: '2006-03-01',
     credentials: {
@@ -30,6 +37,12 @@ const objectStorageService = {
     region: 'us-east-1' // Need to specify valid AWS region or it'll explode ('us-east-1' is default, 'ca-central-1' for Canada)
   }),
 
+  /**
+   * @function deleteObject
+   * Deletes the object at `filePath`
+   * @param {object} options.filePath The filePath of the object
+   * @returns {Promise<object>} The response of the delete object operation
+   */
   deleteObject({ filePath }) {
     const params = {
       Bucket: bucket,
@@ -39,6 +52,12 @@ const objectStorageService = {
     return this._s3Client.send(new DeleteObjectCommand(params));
   },
 
+  /**
+   * @function headObject
+   * Gets the object headers for the object at `filePath`
+   * @param {object} options.filePath The filePath of the object
+   * @returns {Promise<object>} The response of the head object operation
+   */
   headObject({ filePath }) {
     const params = {
       Bucket: bucket,
@@ -48,6 +67,12 @@ const objectStorageService = {
     return this._s3Client.send(new HeadObjectCommand(params));
   },
 
+  /**
+   * @function ListObjectVerseion
+   * Lists the versions for the object at `filePath`
+   * @param {object} options.filePath The filePath of the object
+   * @returns {Promise<object>} The response of the list object version operation
+   */
   listObjectVersion({ filePath }) {
     const params = {
       Bucket: bucket,
@@ -57,11 +82,28 @@ const objectStorageService = {
     return this._s3Client.send(new ListObjectVersionsCommand(params));
   },
 
+  /**
+   * @function presignUrl
+   * Generates a presigned url for the `command` with a limited expiration window
+   * @param {object} [options.expiresIn=300] The number of seconds this signed url will be valid for
+   * @returns {Promise<string>} A presigned url for the direct S3 REST `command` operation
+   */
   presignUrl(command, { expiresIn }) {
     if (!expiresIn) expiresIn = 300; // Default expire to 5 minutes
     return getSignedUrl(this._s3Client, command, { expiresIn });
   },
 
+  /**
+   * @function putObject
+   * Puts the object `stream` at the `id` path
+   * @param {stream} options.stream The binary stream of the object
+   * @param {string} options.id The filePath id of the object
+   * @param {string} options.originalName The original filename of the object
+   * @param {string} options.mimeType The mime type of the object
+   * @param {object} [options.metadata] Optional object containing key/value pairs for metadata
+   * @param {object} [options.tags] Optional object containing key/value pairs for tags
+   * @returns {Promise<object>} The response of the put object operation
+   */
   putObject({ stream, id, originalName, mimeType, metadata, tags }) {
     const params = {
       Bucket: bucket,
@@ -85,6 +127,13 @@ const objectStorageService = {
     return this._s3Client.send(new PutObjectCommand(params));
   },
 
+  /**
+   * @function readObject
+   * Reads the object at `filePath`
+   * @param {object} options.filePath The filePath of the object
+   * @param {object} [options.versionId] Optional specific versionId for the object
+   * @returns {Promise<object>} The response of the get object operation
+   */
   readObject({ filePath, versionId }) {
     const params = {
       Bucket: bucket,
@@ -95,6 +144,13 @@ const objectStorageService = {
     return this._s3Client.send(new GetObjectCommand(params));
   },
 
+  /**
+   * @function readObject
+   * Yields a presigned url for the get object operation with a limited expiration window
+   * @param {object} options.filePath The filePath of the object
+   * @param {object} [options.versionId] Optional specific versionId for the object
+   * @returns {Promise<string>} A presigned url for the direct S3 REST `command` operation
+   */
   readSignedUrl({ filePath, versionId }) {
     const params = {
       Bucket: bucket,
