5eb0a5db29eedfad10d93cba9cb07255084b7f7e
Jeremy Ho <jujaga@gmail.com>
2022-04-08 15:58:14 -0700

    Update redocs html title
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/docs/docs.js
+++ app/src/docs/docs.js
@@ -2,7 +2,7 @@ const docs = {
   getDocHTML: version => `<!DOCTYPE html>
   <html>
     <head>
-      <title>Vue Skeleton API - Documentation ${version}</title>
+      <title>Common Object Management Service API - Documentation ${version}</title>
       <!-- Needed for adaptive design -->
       <meta charset="utf-8"/>
       <meta name="viewport" content="width=device-width, initial-scale=1">
