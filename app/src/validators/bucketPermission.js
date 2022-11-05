a846973129ee79ee48f16d400e7c35c7ed47a91b
Jeremy Ho <jujaga@gmail.com>
2022-11-04 17:06:10 -0700

    Refactor validation layer to split permissions
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/src/validators/bucketPermission.js
@@ -0,0 +1,55 @@
+const { validate, Joi } = require('express-validation');
+const { scheme, type } = require('./common');
+const { Permissions } = require('../components/constants');
+
+const schema = {
+  searchPermissions: {
+    query: Joi.object({
+      userId: scheme.guid,
+      bucketId: scheme.guid,
+      permCode: scheme.permCode
+    }).min(1)
+  },
+
+  listPermissions: {
+    params: Joi.object({
+      bucketId: scheme.guid
+    }),
+    query: Joi.object({
+      userId: scheme.guid,
+      permCode: scheme.permCode
+    })
+  },
+
+  addPermissions: {
+    params: Joi.object({
+      bucketId: type.uuidv4
+    }),
+    body: Joi.array().items(
+      Joi.object().keys({
+        userId: type.uuidv4.required(),
+        permCode: Joi.string().required().valid(...Object.values(Permissions)),
+      })
+    ).required(),
+  },
+
+  removePermissions: {
+    params: Joi.object({
+      bucketId: type.uuidv4
+    }),
+    query: Joi.object({
+      userId: scheme.guid,
+      permCode: scheme.permCode,
+    })
+  }
+};
+
+const validator = {
+  searchPermissions: validate(schema.searchPermissions, { statusCode: 422 }),
+  listPermissions: validate(schema.listPermissions, { statusCode: 422 }),
+  addPermissions: validate(schema.addPermissions, { statusCode: 422 }),
+  removePermissions: validate(schema.removePermissions, { statusCode: 422 }),
+};
+
+module.exports = validator;
+module.exports.schema = schema;
