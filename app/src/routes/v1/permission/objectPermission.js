2fc34ac3a43b6cec6c7f804ee6c0ddffc9e00e85
Jeremy Ho <jujaga@gmail.com>
2022-11-28 09:44:11 -0800

    Rename routes to router for permission endpoints
    Forgot to rename these in a previous standardization.
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/routes/v1/permission/objectPermission.js
+++ app/src/routes/v1/permission/objectPermission.js
@@ -1,4 +1,4 @@
-const routes = require('express').Router();
+const router = require('express').Router();
 
 const { Permissions } = require('../../../components/constants');
 const { objectPermissionController } = require('../../../controllers');
@@ -6,27 +6,27 @@ const { objectPermissionValidator } = require('../../../validators');
 const { checkAppMode, currentObject, hasPermission } = require('../../../middleware/authorization');
 const { requireBasicAuth, requireDb, requireSomeAuth } = require('../../../middleware/featureToggle');
 
-routes.use(checkAppMode);
-routes.use(requireDb);
+router.use(checkAppMode);
+router.use(requireDb);
 
 /** Search for object permissions */
-routes.get('/', requireBasicAuth, objectPermissionValidator.searchPermissions, (req, res, next) => {
+router.get('/', requireBasicAuth, objectPermissionValidator.searchPermissions, (req, res, next) => {
   objectPermissionController.searchPermissions(req, res, next);
 });
 
 /** Returns the object permissions */
-routes.get('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.READ), objectPermissionValidator.listPermissions, (req, res, next) => {
+router.get('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.READ), objectPermissionValidator.listPermissions, (req, res, next) => {
   objectPermissionController.listPermissions(req, res, next);
 });
 
 /** Grants object permissions to users */
-routes.put('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.MANAGE), objectPermissionValidator.addPermissions, (req, res, next) => {
+router.put('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.MANAGE), objectPermissionValidator.addPermissions, (req, res, next) => {
   objectPermissionController.addPermissions(req, res, next);
 });
 
 /** Deletes object permissions for a user */
-routes.delete('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.MANAGE), objectPermissionValidator.removePermissions, (req, res, next) => {
+router.delete('/:objId', requireSomeAuth, currentObject, hasPermission(Permissions.MANAGE), objectPermissionValidator.removePermissions, (req, res, next) => {
   objectPermissionController.removePermissions(req, res, next);
 });
 
-module.exports = routes;
+module.exports = router;
