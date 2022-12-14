0cbd2afac890949b8d3787afa3a1572470ca3957
Jeremy Ho <jujaga@gmail.com>
2022-12-14 12:49:52 -0800

    Change joinPath to use forEach instead of map
    Since the logic does not care about the generated array, we instead use a
    forEach to reduce memory footprint while iterating.
    
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/utils.js
+++ app/src/components/utils.js
@@ -203,12 +203,10 @@ const utils = {
   joinPath(...items) {
     if (items && items.length) {
       const parts = [];
-      items.map(p => {
-        if (p) {
-          p.split('/').map(x => {
-            if (x && x.trim().length) parts.push(x);
-          });
-        }
+      items.forEach(p => {
+        if (p) p.split(DELIMITER).forEach(x => {
+          if (x && x.trim().length) parts.push(x);
+        });
       });
       return parts.join(DELIMITER);
     }
