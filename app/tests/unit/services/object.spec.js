a4f920983834279faa71915b48f4a72a6207fc18
Csaky <Tim.Csaky@gov.bc.ca>
2023-03-02 14:31:57 -0800

    test object service create()

--- app/tests/unit/services/object.spec.js
+++ app/tests/unit/services/object.spec.js
@@ -3,9 +3,12 @@ const { MockModel, MockTransaction } = require('../../common/dbHelper');
 jest.mock('../../../src/db/models/tables/objectModel', () => MockModel);
 
 const service = require('../../../src/services/object');
+const objectPermissionService = require('../../../src/services/objectPermission');
+
+const { Permissions } = require('../../../src/components/constants');
 
 const bucketId = '00000000-0000-0000-0000-000000000000';
-const objectId = '00000000-0000-0000-0000-000000000000';
+const objectId = '00000000-0000-0000-0000-000000000123';
 const userId = '00000000-0000-0000-0000-000000000000';
 
 const data = {
@@ -38,8 +41,8 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-describe('bucket', () => {
-  const createSpy = jest.spyOn(service, 'create');
+describe('object', () => {
+  // const createSpy = jest.spyOn(service, 'create');
   const deleteSpy = jest.spyOn(service, 'delete');
   const getBucketKeySpy = jest.spyOn(service, 'getBucketKey');
   const searchObjectsSpy = jest.spyOn(service, 'searchObjects');
@@ -47,7 +50,7 @@ describe('bucket', () => {
   const updateSpy = jest.spyOn(service, 'update');
 
   beforeEach(() => {
-    createSpy.mockReset();
+    // createSpy.mockReset();
     deleteSpy.mockReset();
     searchObjectsSpy.mockReset();
     getBucketKeySpy.mockReset();
@@ -56,7 +59,7 @@ describe('bucket', () => {
   });
 
   afterAll(() => {
-    createSpy.mockRestore();
+    // createSpy.mockRestore();
     deleteSpy.mockRestore();
     searchObjectsSpy.mockRestore();
     getBucketKeySpy.mockRestore();
@@ -64,10 +67,32 @@ describe('bucket', () => {
     updateSpy.mockRestore();
   });
 
-  it('Create object', async () => {
+  it('add permissions if user', async () => {
+
+    // add userId property
+    const dataWithUser = { ...data, userId: userId };
+
+    // create perms object
+    const perms = Object.values(Permissions).map((p) => ({
+      userId: dataWithUser.userId,
+      permCode: p
+    }));
+
+    // spy on function
+    const addPermissionSpy = jest.spyOn(objectPermissionService, 'addPermissions');
+
+    // mocking the stuff we dont want to test
     const etrx = await jest.fn().mockResolvedValue(MockTransaction);
-    await service.create(data, etrx);
-    expect(createSpy).toHaveBeenCalledTimes(1);
+    addPermissionSpy.mockResolvedValue({});
+
+    // call the function we're testing
+    await service.create(dataWithUser, etrx);
+
+    // the tests:
+    // userId was passed, so addPermissions was called
+    expect(addPermissionSpy).toHaveBeenCalledTimes(1);
+    // addPermissions was called with expected paramters
+    expect(addPermissionSpy).toHaveBeenCalledWith(objectId, perms, userId, etrx);
   });
 
   it('Delete object', async () => {
